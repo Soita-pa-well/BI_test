af864f4aa6249d8bec7ac7e81201e1bac4123b91
Сойта Павел <soita.pavel@yandex.ru>
2024-05-12 12:08:08 +0300

    fix db update

--- main.py
+++ main.py
@@ -62,17 +62,62 @@ for institute in data:
     else:
         insitute_type = 'None'
 
-    query = '''
-    INSERT INTO institutions (name, country, alpha_two_code, state_province,
-                              type)
-    VALUES (%s, %s, %s, %s, %s)
-    '''
+    cur.execute("SELECT * "
+                "FROM institutions "
+                "WHERE name = %s",
+                (institute['name'],)
+                )
+    existing_record = cur.fetchone()
 
-    params = (institute['name'], institute['country'],
-              institute['alpha_two_code'], institute['state-province'],
-              insitute_type)
-    cur.execute(query, params)
-    conn.commit()
+    if existing_record:
+        (ex_id, existing_name, existing_country, existing_alpha,
+         existing_state, existing_type) = existing_record
+        
+        name_changed = existing_name != institute['name']
+        country_changed = existing_country != institute['country']
+        alpha_changed = existing_alpha != institute['alpha_two_code']
+        state_changed = existing_state != institute['state-province']
+        type_changed = existing_type != insitute_type
+
+        if any([name_changed, country_changed, alpha_changed,
+                state_changed]):
+            update_query = '''
+            UPDATE institutions
+            SET name = %s,
+                country = %s,
+                alpha_two_code = %s,
+                state_province = %s,
+                type = %s
+            WHERE name = %s
+            '''
+            update_params = (
+                institute['name'],
+                institute['country'],
+                institute['alpha_two_code'],
+                institute['state-province'],
+                insitute_type,
+                institute['name']
+            )
+            cur.execute(update_query, update_params)
+            conn.commit()
+
+    else:
+        query = '''
+        INSERT INTO institutions (
+            name,
+            country,
+            alpha_two_code,
+            state_province,
+            type)
+        VALUES (%s, %s, %s, %s, %s)
+        '''
+        params = (institute['name'],
+                  institute['country'],
+                  institute['alpha_two_code'],
+                  institute['state-province'],
+                  insitute_type)
+        cur.execute(query, params)
+        conn.commit()
 
 cur.close()
 conn.close()
